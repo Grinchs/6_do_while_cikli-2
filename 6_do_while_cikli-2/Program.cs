23256845f275a4b57e05e1b4aee09fa5c8dbcd7f
Grinchs <marcisg2345@gmail.com>
2023-10-30 23:41:20 +0200

    Add project files.

---
+++ 6_do_while_cikli-2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string pareizaParole = "Gatis";
+
+        Console.WriteLine("Ievadiet paroli: ");
+        string ievaditaParole = Console.ReadLine();
+
+        while (ievaditaParole != pareizaParole)
+        {
+            Console.WriteLine("Parole ir ievadita nepareizi!");
+            ievaditaParole = Console.ReadLine();
+        }
+
+        Console.WriteLine("Parole ir ievadita pareizi!");
+        Console.ReadLine();
+    }
+}
